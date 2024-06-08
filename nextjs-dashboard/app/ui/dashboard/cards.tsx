b536fc265df38dc442ea2eb72c5232c2cddfe578
tutich <pkoinange02@gmail.com>
2024-06-08 12:12:31 +0300

    fetching data

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -17,6 +17,7 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
+      {/* Note: reached this point */}
 
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
